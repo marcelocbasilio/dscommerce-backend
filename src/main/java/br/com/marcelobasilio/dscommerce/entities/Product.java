ceb1c7dd006fec1bce46247b5e23b1108e72560a
Marcelo Basílio <marcelocbasilio@gmail.com>
2023-12-17 13:54:57 -0300

    [tenth-commit] Generating equals and hashCode of entities.

--- src/main/java/br/com/marcelobasilio/dscommerce/entities/Product.java
+++ src/main/java/br/com/marcelobasilio/dscommerce/entities/Product.java
@@ -5,6 +5,7 @@ import org.aspectj.weaver.ast.Or;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -88,5 +89,20 @@ public class Product {
 
     public List<Order> getOrder() {
         return items.stream().map(OrderItem::getOrder).toList();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
