7859251358003eec449a990c5143ea3dba4242b7
Marcelo Basílio <marcelocbasilio@gmail.com>
2023-12-16 22:47:25 -0300

    [first-commit] - Creating the DS Commerce project.

---
+++ src/test/java/br/com/marcelobasilio/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelobasilio.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
