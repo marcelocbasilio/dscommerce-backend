083d53d6a996156c4cce881439c3acd477bda79c
Marcelo Basílio <marcelocbasilio@gmail.com>
2023-12-16 22:50:24 -0300

    [third-commit] - Creating the Order Class.

---
+++ src/main/java/br/com/marcelobasilio/dscommerce/entities/Order.java
@@ -0,0 +1,22 @@
+package br.com.marcelobasilio.dscommerce.entities;
+
+import br.com.marcelobasilio.dscommerce.entities.enums.OrderStatus;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+}
