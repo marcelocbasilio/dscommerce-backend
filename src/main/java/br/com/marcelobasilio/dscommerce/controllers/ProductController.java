b6d2afd3d41f7f92a0f14982787679b16d5e133e
Marcelo Basílio <marcelocbasilio@gmail.com>
2023-12-22 14:07:22 -0300

    [twelfth-commit] List paginated products and product insertion.

--- src/main/java/br/com/marcelobasilio/dscommerce/controllers/ProductController.java
+++ src/main/java/br/com/marcelobasilio/dscommerce/controllers/ProductController.java
@@ -1,16 +1,11 @@
 package br.com.marcelobasilio.dscommerce.controllers;
 
 import br.com.marcelobasilio.dscommerce.dto.ProductDTO;
-import br.com.marcelobasilio.dscommerce.entities.Product;
-import br.com.marcelobasilio.dscommerce.repositories.ProductRepository;
 import br.com.marcelobasilio.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -21,5 +16,15 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         return productService.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return productService.findAll(pageable);
+    }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO productDTO) {
+        return productService.insert(productDTO);
     }
 }
--- src/main/java/br/com/marcelobasilio/dscommerce/services/ProductService.java
+++ src/main/java/br/com/marcelobasilio/dscommerce/services/ProductService.java
@@ -4,6 +4,8 @@ import br.com.marcelobasilio.dscommerce.dto.ProductDTO;
 import br.com.marcelobasilio.dscommerce.entities.Product;
 import br.com.marcelobasilio.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +21,25 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Optional<Product> result = productRepository.findById(id);
         Product product = result.orElseThrow();
+        return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = productRepository.findAll(pageable);
+        return result.map(ProductDTO::new);
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO productDTO) {
+        Product product = new Product();
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setPrice(productDTO.getPrice());
+        product.setImgUrl(productDTO.getImgUrl());
+
+        product = productRepository.save(product);
+
         return new ProductDTO(product);
     }
 }
